4601328a48152b7509f62ed3007595c32e67ff10
rony <elfantasmax2146@gmail.com>
2021-02-15 17:50:56 -0500

    proyecto finalizado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,61 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registrarImagen } from "./lazy"
 
-console.log('Happy hacking :)')
+const maximum = 122
+const minimum = 1
+
+let totalImages = 0
+
+const get = select => document.querySelector(select)
+const crear = select => document.createElement(select)
+const getRandom = () => Math.floor(Math.random() * ( maximum - minimum )) + minimum
+const buttonAdd = get('button')
+const app = get('#images')
+const Limpiar = get('#limpiar')
+
+const AddImage = () => app.append( createImageComponent() )
+const CleanDOM = () => 
+{
+    let i = 0
+    while ( i < app.childElementCount ){
+        app.lastChild.remove()
+    }
+}
+
+buttonAdd.addEventListener( 'click' , AddImage )
+Limpiar.addEventListener( 'click' , CleanDOM )
+
+const createImageComponent = () => 
+{
+
+    const imagen = crear('img')
+    imagen.className = 'mx-auto'
+    imagen.width = '320'
+    // imagen.src = url
+    imagen.dataset.src = `https://randomfox.ca/images/${getRandom()}.jpg`
+    registrarImagen(imagen )
+
+    const container = crear('div')
+    container.className = "p-4 bg-gray-300 border border-transparent rounded-md"
+    container.style.minHeight = "100px";
+    container.style.maxWidth = "350px";
+    container.style.margin = "auto";
+    container.style.marginBottom = "30px";
+    // container.style.display = "inline-block";
+    container.append( imagen  )
+    
+    // LE APLICAMOS EL INTERSECTION OBSERVER
+    totalImages+=1
+    
+    console.log(`⚪ Se han agregado ${totalImages} imágenes`);
+
+    return container
+
+}
+
+const RenderDOM = () => {
+    
+    app.append( createImageComponent() )
+    
+}
+
+RenderDOM()
